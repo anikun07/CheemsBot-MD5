93ba9a0d4a609aff23ddb7658e35070619d4b937
anikun07 <127094377+anikun07@users.noreply.github.com>
2023-03-06 20:33:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,26 +14,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['919832518780'] //ur owner number
+global.ownername = ["ani"] //ur owner name
+global.ytname = "YT: ani" //ur yt chanel name
+global.socialm = "GitHub: ani" //ur github or insta name
+global.location = "India, West Bengal, Siliguri" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213'] //ur number
-global.ownernomer = "916909137213" //ur number
-global.ownertag = '916909137213' //ur tag number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot V5' //ur bot name
-global.ownername = "🦄Dream Guy Xeon"
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD5' //script link
+global.owner = ['919832518789'] //ur number
+global.ownernomer = "919832518780" //ur number
+global.ownertag = '919832518780' //ur tag number
+global.premium = ['919832518780'] //ur premium number
+global.botname = 'Cheems Bot V6' //ur bot name
+global.ownername = "Anirudh Roy"
+global.linkz = "https://youtu.be/ani" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtu.be/ani" //ur website to be displayed
+global.botscript = 'https://github.com/ani0609/CheemsBot-MD5' //script link
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.packname = "Sticker by Anirudh" //ur sticker watermark packname
+global.author = "doge not" //ur sticker watermark author
+global.wm = "ani bot inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
